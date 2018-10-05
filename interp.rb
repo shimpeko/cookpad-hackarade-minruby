38cf7e8104122a149401b01b3539f11d13bbe292
Shimpei Kodama <shimpei-kodama@cookpad.com>
2018-10-05 13:41:26 +0900

    Pass test6-3

--- interp.rb
+++ interp.rb
@@ -179,16 +179,16 @@ def evaluate(exp, env)
 
   # You don't need advices anymore, do you?
   when "ary_new"
-    raise(NotImplementedError) # Problem 6
+    exp[1..-1].each_with_index.map {|i, idx| [idx, evaluate(i, env)] }.to_h
 
   when "ary_ref"
-    raise(NotImplementedError) # Problem 6
+    evaluate(exp[1], env)[evaluate(exp[2], env)]
 
   when "ary_assign"
-    raise(NotImplementedError) # Problem 6
+    evaluate(exp[1], env)[evaluate(exp[2], env)] = evaluate(exp[3], env)
 
   when "hash_new"
-    raise(NotImplementedError) # Problem 6
+    Hash[*(exp[1..-1].map {|i| evaluate(i, env)})]
 
   else
     p("error")
